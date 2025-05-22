f10f6e2d29c6f3119881ccc53534ec29766646ad
Patrik <Muravski.patrik@gmail.com>
2025-05-22 16:45:00 +0300

    began a project creation

--- agent-interactor/main.py
+++ agent-interactor/main.py
@@ -1,6 +1,27 @@
+from langchain.core.messages import HumanMessage # high level framework to build LLM applications
+from langchain_openai import ChatOpenAI # allows to use OpenAI's LLMs within LangChain & langgraph
+from langchain.tools import tool
+from langgraph.prebuilt import create_react_agent # complex framework to build AI agents
+from dotenv import load_dotenv 
+
+# Ai agent has a access to a set of tools
+
+load_dotenv()
+
 def main():
-    print("Hello from agent-interactor!")
+    model = ChatOpenAi(temperature=0)
 
+    tools = []
+    agent_executor = create_react_agent(model, tools)
 
-if __name__ == "__main__":
-    main()
+    print("Welcome to the AI agent. Type 'exit' to quit.")
+
+    while True:
+        user_input = input("\nYou: ").strip()
+
+        if user_input.lower() == "exit":
+            break
+
+        print("\nAssistant")
+
+        
